1d630bf337c06330f5322bec67218524781592a7
yvandouenel <y.douenel@coopernet.fr>
2023-04-19 09:52:05 +0200

    Exo split corrigé.

--- js/js/11exoSplit.js
+++ js/js/11exoSplit.js
@@ -1,11 +1,19 @@
-class CustomString extends String {
-  split(separator) {
-    return super.split(separator);
+/**
+ * Class CustomString qui étend String
+ */
+class CustomString extends String{
+  split(limiter){
+    const initial_table =  super.split(limiter);
+    // Utilisation de map pour créer le deuxième tableau qui ajoute le limiter sauf sur le dernier élément
+    const final_table = initial_table.map((elt,index)=>{
+      if(index === (initial_table.length - 1)) return elt;
+      return elt + limiter;
+    });
+    return [
+      initial_table,
+      final_table
+    ]
   }
 }
-
 const hello = new CustomString("Hello World");
-
-const result = hello.split("o");
-
-console.log(`Retout de hello.split("o")`, result);
+console.log(`resultat : `, hello.split("o"));
