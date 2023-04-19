9743e5f5e1700102d6530488577339bee6cc6bb3
yvandouenel <y.douenel@coopernet.fr>
2023-04-19 14:40:22 +0200

    Correction de l'exercice sur les boutons avec createMarkup.

--- js/js/15ExoCreateDom.js
+++ js/js/15ExoCreateDom.js
@@ -7,7 +7,7 @@
    * @param {Object} attribute  (doit comprendre les propriétés name et value)
    * @returns domElement
    */
-function createMarkup(markup_name, text, parent, attribute) {
+/* function createMarkup(markup_name, text, parent, attribute) {
   const markup = document.createElement(markup_name);
   markup.textContent = text;
   parent.appendChild(markup);
@@ -16,5 +16,40 @@ function createMarkup(markup_name, text, parent, attribute) {
   }
   return markup;
 }
-// Création d'un élément du DOM section
-const section = createMarkup("section", "Test de texte", document.body, { name: "class", value: "toto" });
+// Création des éléments du  DOM via la fonction createMarkup
+const header = createMarkup('header', "", document.body);
+const nav = createMarkup('nav', '', header);
+for (let i = 0; i < 4; i++) {
+  createMarkup('button', `Item ${i + 1}`, header);
+}
+document.querySelector("button").style.color = "red"; */
+
+/**
+   * Crée un élément du dom, lui ajoute du texte, le place comme dernier
+   * enfant de parent et ajoute un attribut en utilisant le paramètre attribute
+   * @param {String} markup_name 
+   * @param {String} text 
+   * @param {domElement} parent 
+   * @param {Array} attributes  (doit comprendre les propriétés name et value)
+   * @returns domElement
+   */
+function createMarkup(markup_name, text, parent, attributes = []) {
+  const markup = document.createElement(markup_name);
+  markup.textContent = text;
+  parent.appendChild(markup);
+  attributes.forEach((attribute) => {
+    if (attribute && attribute.hasOwnProperty("name") && attribute.hasOwnProperty("value")) {
+      markup.setAttribute(attribute.name, attribute.value);
+    }
+  })
+
+  return markup;
+}
+// Création des éléments du  DOM via la fonction createMarkup
+const header = createMarkup('header', "", document.body);
+const nav = createMarkup('nav', '', header);
+for (let i = 0; i < 4; i++) {
+  createMarkup('button', `Item ${i + 1}`, header, [{ name: "class", value: "danger" }, { name: "id", value: "danger" + i }]);
+}
+document.querySelector("button").style.color = "red";
+
