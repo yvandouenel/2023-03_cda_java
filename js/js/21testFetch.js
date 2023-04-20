21be3f0278ca98791ae13476044e191568d789a4
yvandouenel <y.douenel@coopernet.fr>
2023-04-20 15:28:54 +0200

    Exo univ en cours.

--- js/js/21testFetch.js
+++ js/js/21testFetch.js
@@ -1,11 +1,44 @@
-fetch("http://universities.hipolabs.com/search?country=Italy")
-.then(response => {
-  console.log(`statut de la response `, response.status);
-  return response.json();
-})
-.then(universities => {
-  console.log(`univ :  `, universities);
-})
-.catch(error => {
-  console.error(`erreur attrapée : `, error);
-})
+import createMarkup from "./utils/utils.js";
+const form = document.querySelector("form");
+
+form.onsubmit = function (e) {
+  console.log(`Dans onsubmit`);
+  e.preventDefault();
+  let country = document.querySelector("#country").value;
+  console.log(`country`, country);
+  // appel du endpoint via fetch
+  getUniversities(country);
+}
+
+function getUniversities(country) {
+  return fetch(`http://universities.hipolabs.com/search?country=${country}`)
+    .then((response) => {
+      if (response.status !== 200) {
+        throw new Error("Le serveur ne répond pas !");
+      } else return response.json();
+    })
+    .then((data) => console.log(`contenu du resolve de la promesse : `, data))
+    .catch((error) => console.log(`Erreure attrapée : `, error));
+}
+
+// Gestion de l'événement submit du formulaire pour qu'il ne recharge pas la page 
+
+/* const api = fetch("http://universities.hipolabs.com/search")
+  .then((response) => {
+    if (response.status !== 200) {
+      throw new Error("Le serveur ne répond pas !");
+    } else return response.json();
+  })
+  .then((data) => console.log(`contenu du resolve de la promesse : `, data))
+  .catch((error) => console.log(`Erreure attrapée : `, error));
+
+let fieldset = createMarkup("fieldset", null, form, [{name:"id", value:"select-fieldet"}])
+let select = createMarkup("select", null, fieldset, [
+  { name: "name", value: `country-select` },
+]);
+
+for (let item of api.then(data => data.json())) {
+  createMarkup("option", `${item.country}`, select, [
+    { name: "value", value: "" },
+  ]);
+} */
